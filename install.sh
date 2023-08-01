6aaf96415a495d553838804be02e6b1a9db08b86
melt <thomas.meltzer1@gmail.com>
2023-08-01 16:23:58 +0200

    update install script

--- install.sh
+++ install.sh
@@ -8,24 +8,56 @@ read choice
 
 if [ "$choice" = "I" ]
 then
-	# install dot files
-	#==================
-	echo "Copying dot files to $HOME"
-	for i in $files; do ln -sv ~/.dotfiles/$i $HOME/$i ; done
 
-	# setup vundle submodule
-	#=======================
-	echo "Installing vundle submodule..."
-    mkdir -p .vim/bundle/
-    git submodule add -f https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
-	echo "complete"
+    cd $HOME
+
+    echo "Copying dot files to $HOME"
+    for i in $files; do ln -sfv $HOME/.dotfiles/$i $HOME/$i ; done
+    echo "complete"
+
+    echo "Installing vundle submodule..."
+    folder=$HOME/.vim/bundle
+    if [ -d "$folder" ]; then
+        echo "skipping... $folder already exists."
+    else
+        mkdir -p $folder
+        cd $folder
+        git submodule add -f https://github.com/VundleVim/Vundle.vim.git Vundle.vim
+        cd $HOME
+
+        echo "run the following to install plugins in vim:"
+        echo "$ vim -c PluginInstall"
+    fi
+    echo "complete"
+
+    echo "installing miniconda"
+    folder=$HOME/miniconda3
+    if [ -d "$folder" ]; then
+        echo "skipping... $folder already exists."
+    else
+        cd ~
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+        bash Miniconda3-latest-Linux-x86_64.sh -b
+        $folder/bin/conda init bash
+        $folder/bin/conda config --set auto_activate_base false
+    fi
+    echo "complete"
+
+    echo "installing fzf"
+    folder=$HOME/.fzf
+    if [ -d "$folder" ]; then
+        echo "skipping... $folder already exists."
+    else
+        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+        ~/.fzf/install --all
+    fi
+    echo "complete"
+
 
 elif [ "$choice" = "R" ]
 then
-	# remove dot files
-	#==================
-	echo "removing dot files from $HOME"
-	for i in $files; do rm -ivr $HOME/$i ; done
+    echo "removing dot files from $HOME"
+    for i in $files; do rm -ivr $HOME/$i ; done
 else
-	echo "Option not supported. Please try again."
+    echo "Option not supported. Please try again."
 fi
